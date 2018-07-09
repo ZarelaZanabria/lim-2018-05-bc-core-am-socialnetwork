240830bff9cee9a60ae52ae5c91601f7a19e1ce0
noelyGitHub <noely.alis209@gmail.com>
2018-07-09 16:58:50 -0400

    register sin authetication

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   alert("empezamos!!");
 // Initialize Firebase
-/*var config = {
+var config = {
   apiKey: "AIzaSyBwy0way-XvpLQTkY2EoKz5uSHhb23S3fo",
   authDomain: "pruebasalud-8632b.firebaseapp.com",
   databaseURL: "https://pruebasalud-8632b.firebaseio.com",
@@ -9,26 +9,25 @@ window.onload = function() {
   storageBucket: "pruebasalud-8632b.appspot.com",
   messagingSenderId: "384943209525"
 };
-firebase.initializeApp(config);*/
+firebase.initializeApp(config);
 // get elements
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
 const btnLogin = document.getElementById('btnLogin');
 const btnSignUp = document.getElementById('btnSignUp');
 const btnLogOut = document.getElementById('btnLogOut');
+const emailUsers = document.getElementById('users-email');
+const passwordUsers= document.getElementById('users-password');
 const btnLoginFacebook = document.getElementById('btnLoginFacebook');
 const btnLoginGoogle = document.getElementById('btnLoginGoogle');
+
 // add login events
 //....................................................................... AUNTENTIFICACION CON CORREO Y CONTRASENIA
 btnSignUp.addEventListener('click', e => {
-  alert("hola me estoy registrando");
-  // get sign up user
-  const email = txtEmail.value;// validar correo
-  const password = txtPassword.value;
-  const auth = firebase.auth();
-  const promise = auth.createUserWithEmailAndPassword(email , password);// devuelve una promesa que permita identificar al usuario o para detectar cualquien error y registrarlos en firebase
-  promise.catch(e => console.log(e.message));// para detectar un error
-  // DONE: validar si ya esta registrado con el email
+ 
+ document.getElementById('section-register-user').style.display='block';
+document.getElementById('section-login').style.display='none';
+
 });
 // add a realtime listener
 /*Esto me ayuda saber cada vez que cambie el estado de autentificacion
@@ -37,6 +36,8 @@ btnSignUp.addEventListener('click', e => {
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser){// si existe el usuario lo insertamos a firebase console
       console.log(firebaseUser);
+      console.log(typeof(firebaseUser));
+      console.log('informacion del usuario logueado: '+firebaseUser.displayName);// UTILIZO PARA LA BIENBENIDA
       btnLogOut.style.display = 'block';
   }else{// si no mostramos un mensaje de no regstrado 
       console.log('No registrado');
@@ -68,7 +69,8 @@ btnLoginGoogle.addEventListener('click', e => {
     firebase.auth (). signInWithPopup (provider) .then (function (result) {// Autentica un cliente de Firebase usando un flujo de autenticación OAuth basado en ventanas emergentes.
         var token = result.credential.accessToken;
         var user = result.user;//info 
-        console.log('informacion del usuario logueado: '+user)
+        console.log(typeof(user));
+        console.log('informacion del usuario logueado: '+user.displayName)
       }). catch (error => {
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -92,6 +94,7 @@ btnLoginFacebook.addEventListener('click', e => {
     firebase.auth (). signInWithPopup (provider) .then (function (result) {
       var token = result.credential.accessToken;
       var user = result.user;// La información del usuario que inició sesión.
+      Console.log(user);
       //console.log('Informacion del usuario logueado con facebok'+user);
     }). catch (error => {
       var errorCode = error.code;
@@ -100,27 +103,51 @@ btnLoginFacebook.addEventListener('click', e => {
       var credential = error.credential;
     });
 });
+//...................................................................................REGISTRO DE USUARIO
+//Inicializamos una variable global.
+let formUsers;
+let refUsers;
+let tbodyTableUsers;
+let elementDeleteUser;
+//Funcion en donde vamos a Ejecutar el registro de usuario
 
+    //Seleccionamos el formulario Usuario.
+    formUsers = document.getElementById('form-users');
+    //Creamos un evento en donde al hacer click ejecutamos la funcion ubmitUsersFirebase
+ /*  Indicamos la base de datos que vamos conectar que 
+    Hace referente al hijo del modo raiz de la base de datos */
+refUsers = firebase.database().ref().child('Usuarios');
+buttonR=this.document.getElementById('register');
+lastNameUser=document.getElementById('users-last-name');
+nameUser=document.getElementById('users-name');
 
-//...............................................................................Administrando database en tiempo real
-const object = document.getElementById('Object');
-// creando referencia
-var dbRefObject =firebase.database().ref().child('object')// ref nos lleva a la raiz de la base de datos y al llamarla child crea una hija del object
-// sincronizo 
-dbRefObject.on('value',function(data) {// Metodo on lee los datos de una ruta de acceso y value detecta cambios en el contenido de la ruta y val contiene los datos
-  console.log("hoaaaal");// que coños tienes buaaaaaa!!!
+buttonR.addEventListener('click', event => {   debugger 
+  const email =emailUsers.value;// validar correo
+  const password = passwordUsers.value;
+  const auth = firebase.auth();
+  const promise = auth.createUserWithEmailAndPassword(email , password);// devuelve una promesa que permita identificar al usuario o para detectar cualquien error y registrarlos en firebase
+  promise.catch(e => console.log(e.message));// para detectar un error
+  // DONE: validar si ya esta registrado con el email
+    //Para borrar una vez que guarda los archivos
+    refUsers.push({
+        usersEmail: email,
+        usersLastName: lastNameUser.value,
+        usersName: nameUser.value,
+        usersPassword: password,
+    });
+
+  // e.preventDefault();
+  // get sign up user
+
+    formUsers.reset();
+  refUsers.on('value',function (snap) {
+    //Vamos a obtener los valores de la base de datos Ususer
+    let dataUsers = snap.val();
+  console.log(dataUsers); 
+    console.log(refUsers[-LH-nClrH0D_GjH6aHrk].usersName);
+  
+  });
 });
-/* INSERTAR USUARIO
-function writeUserData(userId, name, email, imageUrl) {
-firebase.database().ref('users/' + userId).set({
-  username: name,
-  email: email,
-  profile_picture : imageUrl
-});
-} */
-
-
-
 
 
 
--- src/js/conexionUsers.js
+++ src/js/conexionUsers.js
@@ -1,4 +1,4 @@
-var config = {
+/*var config = {
     apiKey: "AIzaSyBwy0way-XvpLQTkY2EoKz5uSHhb23S3fo",
     authDomain: "pruebasalud-8632b.firebaseapp.com",
     databaseURL: "https://pruebasalud-8632b.firebaseio.com",
@@ -6,4 +6,4 @@ var config = {
     storageBucket: "pruebasalud-8632b.appspot.com",
     messagingSenderId: "384943209525"
   };
-  firebase.initializeApp(config);
+  firebase.initializeApp(config);*/
