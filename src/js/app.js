44579709231a001e9b0ef25f05161b8678a5c21f
noelyGitHub <noely.alis209@gmail.com>
2018-07-10 14:33:07 -0400

    validacion de resgistro con Autentificacion firebase

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 window.onload = function () {
   // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBwy0way-XvpLQTkY2EoKz5uSHhb23S3fo",
-  authDomain: "pruebasalud-8632b.firebaseapp.com",
-  databaseURL: "https://pruebasalud-8632b.firebaseio.com",
-  projectId: "pruebasalud-8632b",
-  storageBucket: "pruebasalud-8632b.appspot.com",
-  messagingSenderId: "384943209525"
-};
-firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyBwy0way-XvpLQTkY2EoKz5uSHhb23S3fo",
+    authDomain: "pruebasalud-8632b.firebaseapp.com",
+    databaseURL: "https://pruebasalud-8632b.firebaseio.com",
+    projectId: "pruebasalud-8632b",
+    storageBucket: "pruebasalud-8632b.appspot.com",
+    messagingSenderId: "384943209525"
+  };
+  firebase.initializeApp(config);
 
   // get elements
   const txtEmail = document.getElementById('txtEmail');
@@ -18,8 +18,10 @@ firebase.initializeApp(config);
   const btnLogOut = document.getElementById('btnLogOut');
   const emailUsers = document.getElementById('users-email');
   const passwordUsers = document.getElementById('users-password');
+  const passwordTwoUsers = document.getElementById('users-passwordTwo');
   const btnLoginFacebook = document.getElementById('btnLoginFacebook');
   const btnLoginGoogle = document.getElementById('btnLoginGoogle');
+
   let refUsers;
   let tbodyTableUsers;
   let elementDeleteUser;
@@ -28,20 +30,45 @@ firebase.initializeApp(config);
   let nameUser = document.getElementById('users-name');
   let userLogin;
   let emailValidate;
+  let passwordValidate;
   //....................................................................... VALIDACION DE CORREO ELECTRONICO
- txtEmail.addEventListener('input', function() {
+  txtEmail.addEventListener('input', function () {
     campo = event.target;
-    val=campo.value;
+    val = campo.value;
     emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     //Se muestra un texto a modo de ejemplo, luego va a ser un icono
     if (emailRegex.test(val)) {
-      
-      document.getElementById('icon-check').setAttribute('class','icon-checkmark');
+      document.getElementById('icon-check').setAttribute('class', 'icon-checkmark');
     } else {
-      document.getElementById('icon-check').setAttribute('class','icon-cross');
+      document.getElementById('icon-check').setAttribute('class', 'icon-cross');
     }
-});
-  
+  });
+  emailUsers.addEventListener('input', function () {
+    campo = event.target;
+    val = campo.value;
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    if (emailRegex.test(val)) {
+      emailValidate = 'emailValido';
+      document.getElementById('icon-validate').setAttribute('class', 'icon-checkmark');
+    } else {
+      emailValidate = 'emailInvalido';
+      document.getElementById('icon-validate').setAttribute('class', 'icon-cross');
+    }
+  });
+  //....................................................................... VALIDACION DE PASSWORD REPETIDO
+  passwordTwoUsers.addEventListener('input', function () {
+    passwordTwo = event.target.value;
+    passwordOne = passwordUsers.value;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    if (passwordTwo === passwordOne) {
+      passwordValidate = 'passwordValido';
+      document.getElementById('icon-validate-password').setAttribute('class', 'icon-checkmark');
+    } else {
+      passwordValidate = 'passwordInvalido';
+      document.getElementById('icon-validate-password').setAttribute('class', 'icon-cross');
+    }
+  });
   //....................................................................... AUNTENTIFICACION CON CORREO Y CONTRASENIA
   btnSignUp.addEventListener('click', e => {
     document.getElementById('section-register-user').style.display = 'block';
@@ -57,6 +84,7 @@ firebase.initializeApp(config);
       userLogin = firebaseUser.displayName;
       console.log(firebaseUser);
       console.log('informacion del usuario logueado: ' + firebaseUser.displayName);// UTILIZO PARA LA BIENBENIDA
+      document.getElementById('messageValide').innerHTML = '';//  limpio el elemento que notofica si es email y passwor correcto
       btnLogOut.style.display = 'block';
     } else {// si no mostramos un mensaje de no regstrado 
       console.log('No registrado');
@@ -65,15 +93,14 @@ firebase.initializeApp(config);
   });
   // Iniciar Sesion
   btnLogin.addEventListener('click', e => {
-     if(emailValidate === 'valido'){ 
-      const auth = firebase.auth();
-      const promise = auth.signInWithEmailAndPassword(txtEmail.value, txtPassword.value);// devuelve una promesa que permita identificar al usuario o para detectar cualquien error y registrarlos en firebase
-      promise.catch(e => console.log(e.message));// para detectar un error
-      //DONE: validar si los datos son existentes 
-     }else{
-      document.getElementById('messageValide').innerHTML='Ingrese un correo valido'
-    } 
-   
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(txtEmail.value, txtPassword.value);// devuelve una promesa que permita identificar al usuario o para detectar cualquien error y registrarlos en firebase
+    promise.catch(e => {
+      console.log(e.message);
+      return document.getElementById('messageValide').innerHTML = 'Email o contraseña incorrecta'
+    });//Muestra un mensaje de email y pass invalido
+    //DONE: validar si los datos son existentes 
+
   });
   // Cerrar Sesion
   btnLogOut.addEventListener('click', e => {
@@ -129,29 +156,41 @@ firebase.initializeApp(config);
   refUsers = firebase.database().ref().child('Usuarios');
   //evento Registrar
   buttonR.addEventListener('click', event => {
-    debugger
-    //Insertar utentificacion
-    const auth = firebase.auth();
-    const promise = auth.createUserWithEmailAndPassword(emailUsers.value, passwordUsers.value).then(function(user) {
-      return user.updateProfile({'displayName':nameUser.value});
-    }).catch(function(error) {
-      console.log(error);
-    });  
-    promise.catch(e => console.log(e.message));
+      if (passwordValidate === 'passwordValido') {
+        
+          //Insertar utentificacion
+          const auth = firebase.auth();
+          const promise = auth.createUserWithEmailAndPassword(emailUsers.value, passwordUsers.value).then(function (user) {
+            return user.updateProfile({ 'displayName': nameUser.value });
+          }).catch(function (error) {
+            console.log(error);
+            var errorEmail=error.message;
+            if(errorEmail === 'Password should be at least 6 characters'){
+               document.getElementById('mensaggeRegisterValide').innerHTML = 'La contraseña debe ser mayor a 6';
+            }
+            if(errorEmail === 'The email address is badly formatted.'){
+              document.getElementById('mensaggeRegisterValide').innerHTML = 'Ingrese un correo valido';
+              }
+          });
+          promise.catch(e => console.log(e.message));
+          //Insertar a la base de datos
+          refUsers.push({
+            usersEmail: emailUsers.value,
+            usersLastName: lastNameUser.value,
+            usersName: nameUser.value,
+            usersPassword: passwordUsers.value,
+          });
+          //formUsers.reset();
+          refUsers.on('value', function (snap) {
+            let dataUsers = snap.val();
+            console.log(dataUsers);
+          });
+        
+      } else {
+        document.getElementById('mensaggeRegisterValide').innerHTML = 'Las contraseñas deben ser iguales';
+      }
+    
 
-    //Insertar a la base de datos
-    refUsers.push({
-      usersEmail: emailUsers.value,
-      usersLastName: lastNameUser.value,
-      usersName: nameUser.value,
-      usersPassword: passwordUsers.value,
-    });
-    //formUsers.reset();
-    refUsers.on('value', function (snap) {
-      let dataUsers = snap.val();
-      console.log(dataUsers);
-
-    });
   });
 
 
