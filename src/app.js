569d33bbbd88647cff27edc8b6a84ecfbd38ed13
Elizabeth Zarela Zanabria Chuquipiondo <zarelixzana@gmail.com>
2018-07-22 20:27:37 -0500

    Testing

--- src/app.js
+++ src/app.js
@@ -1,46 +1,40 @@
-//Primer paso...ver si el usuario esta logeado o no logeado
 // ..........................................................................estado de autentificacion en tiempo real
-
-
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
         console.log(firebaseUser);
         let userLogin = firebaseUser.displayName;
         let photoUser = firebaseUser.photoURL;
+        let email = firebaseUser.email;
         let componente;
         if(photoUser != null){
-            componente = headerElement(userLogin, photoUser);
+            componente = headerElement(userLogin, photoUser,email);
         }else{
             componente = headerElement(userLogin,'http://svgur.com/i/65U.svg');
         }
         
-      /*   $('#header-main').show(); */
-        $('#header-main').append(componente).show();
+        $('#header-main').show();
+        $('#header-main').append(componente);
         $('#post-main').show();
         $('#newPost').append(sectionElement());
         $('#btnLogOut').show(); // aparece mi boton salir
         // login y register oculto
-        $('#portada').remove();
+        $('#portada').hide();
         $('#contentLoginRegister').hide();
         $('#section-register-user').remove();
         $('#section-login').remove();      
-        /* viewPost (); 
-        eventsPost(); */
-        
+        viewPost (); 
+        eventsPost();
 
     } else {// si no mostramos un mensaje de no regstrado 
-        $('#portada').show();
         $('#header-main').hide();
         $('#header-main-content').remove();
         loginElement();
-        $('#portada').show();
         $('#contentLoginRegister').show();
         $('#post-main').hide();
         registerElement();
-        /* document.getElementById('post-main').innerHTML = '';
-        document.getElementById('header-main').innerHTML = ''; */
+        document.getElementById('post-main').innerHTML = '';
+        document.getElementById('header-main').innerHTML = '';
         console.log('No Autentificado');
         eventsLogin();// todos los eventos con login
     }
 });
-
---
+++ src/conexion.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDdzJtiOwXB13ksX1j7MbVUJcS1WilTe-o",
+    authDomain: "modomama-af9f1.firebaseapp.com",
+    databaseURL: "https://modomama-af9f1.firebaseio.com",
+    projectId: "modomama-af9f1",
+    storageBucket: "modomama-af9f1.appspot.com",
+    messagingSenderId: "22604766458"
+  };
+  firebase.initializeApp(config);
