005dd73e186cb35ff1cfbe5ff378cd14c6d8b284
ZarelixCoder <zarelixzana@gmail.com>
2018-07-29 10:54:19 -0500

    cree kuna funcion main/deletePost

--- src/js/main/mainDeletePost.js
+++ src/js/main/mainDeletePost.js
@@ -1,17 +1,13 @@
 const eventsDeletePost = () => {
-    $('.delete-post').click(() => {
-      let dataDeletePost = $('.delete-post').attr('data-posts');
-      let userId = firebase.auth().currentUser;
-      let refDeletePost = firebase.database().ref('posts/' + dataDeletePost);
-      let refDeletePostUser = firebase.database().ref('user-posts/'+userId.uid+'/'+ dataDeletePost);
-      refDeletePost.remove();
-      refDeletePostUser.remove();
-      document.getElementById('div_new_post').innerHTML = '';
-      $("#div_new_post").hide();
-      
-    });
-    document.getElementById("hide_form_search").addEventListener('click', function () {
-      document.getElementById('div_new_post').innerHTML = '';
-      $("#div_new_post").hide();
-    }, false);
-  }
+  $('.delete-post').click(() => {
+    let dataDeletePost = $('.delete-post').attr('data-posts');
+    deletePost(dataDeletePost);     
+    document.getElementById('div_new_post').innerHTML = '';
+    $("#div_new_post").hide();
+    
+  });
+  document.getElementById("hide_form_search").addEventListener('click', function () {
+    document.getElementById('div_new_post').innerHTML = '';
+    $("#div_new_post").hide();
+  }, false);
+}
